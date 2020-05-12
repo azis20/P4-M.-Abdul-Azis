4fbd914fd6fdba263938ecfde00e65d03dbd7d01
azis20 <64583776+azis20@users.noreply.github.com>
2020-05-12 14:25:43 +0800

    Create Jobsheet 2 C++.cpp
    Menampilkan array dua dimensi

---
+++ Jobsheet 2 C++.cpp
@@ -0,0 +1,46 @@
+#include <iostream> 
+using namespace std;
+int main() { 
+
+string NIM [13]; 
+    cout<<"Input NIM Mahasiswa ke-1 = "; 
+    cin>>NIM[0];  
+    cout<<"Input NIM Mahasiswa ke-2 = ";  
+    cin>>NIM[1]; 
+    cout<<"Input NIM Mahasiswa ke-3 = ";  
+    cin>>NIM[2]; 
+    cout<<"Input NIM Mahasiswa ke-4 = ";  
+    cin>>NIM[3];  
+    cout<<"Input NIM Mahasiswa ke-5 = ";  
+    cin>>NIM[4]; 
+    cout<<"Input NIM Mahasiswa ke-6 = ";  
+    cin>>NIM[5]; 
+	cout<<"Input NIM Mahasiswa ke-7 = ";  
+    cin>>NIM[6]; 
+    cout<<"Input NIM Mahasiswa ke-8 = ";  
+    cin>>NIM[7]; 
+    cout<<"Input NIM Mahasiswa ke-9 = ";  
+    cin>>NIM[8]; 
+    cout<<"Input NIM Mahasiswa ke-10 = ";  
+    cin>>NIM[9]; 
+    cout<<"Input NIM Mahasiswa ke-11 = ";  
+    cin>>NIM[10]; 
+    cout<<"Input NIM Mahasiswa ke-12 = ";  
+    cin>>NIM[11]; 
+    cout<<"Input NIM Mahasiswa ke-13 = ";  
+    cin>>NIM[12]; 
+	cout<<endl;
+	cout<<"NIM Mahasiswa ke-1 adalah "<<NIM[0]<<endl;  
+    cout<<"NIM Mahasiswa ke-2 adalah "<<NIM[1]<<endl;  
+	cout<<"NIM Mahasiswa ke-3 adalah "<<NIM[2]<<endl;  
+	cout<<"NIM Mahasiswa ke-4 adalah "<<NIM[3]<<endl; 
+	cout<<"NIM Mahasiswa ke-5 adalah "<<NIM[4]<<endl; 
+	cout<<"NIM Mahasiswa ke-6 adalah "<<NIM[5]<<endl;
+	cout<<"NIM Mahasiswa ke-7 adalah "<<NIM[6]<<endl;
+	cout<<"NIM Mahasiswa ke-8 adalah "<<NIM[7]<<endl;
+	cout<<"NIM Mahasiswa ke-9 adalah "<<NIM[8]<<endl;
+	cout<<"NIM Mahasiswa ke-10 adalah "<<NIM[9]<<endl;
+	cout<<"NIM Mahasiswa ke-11 adalah "<<NIM[10]<<endl;
+	cout<<"NIM Mahasiswa ke-12 adalah "<<NIM[11]<<endl;
+	cout<<"NIM Mahasiswa ke-13 adalah "<<NIM[12]<<endl;
+ } 
