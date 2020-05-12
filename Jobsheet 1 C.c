8b3a025bac7d3d6bca00c4670407096e069bcf45
azis20 <64583776+azis20@users.noreply.github.com>
2020-05-12 14:19:07 +0800

    Create Jobsheet 1 C.c
    Menampilkan array satu dimensi

---
+++ Jobsheet 1 C.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+	printf("Nama: M.Abdul Azis\n");
+    printf("NIM: F1B019083\n");
+    printf("kelompok: 17\n");
+    
+    int NIM [13]= {1,2,3,4,5,6,7,8,9,10,11,12,13};
+    printf("\nNIM 1 = %d", NIM[0]);
+    printf("\nNIM 2 = %d", NIM[1]);
+    printf("\nNIM 3 = %d", NIM[2]);
+    printf("\nNIM 4 = %d", NIM[3]);
+    printf("\nNIM 5 = %d", NIM[4]);
+    printf("\nNIM 6 = %d", NIM[5]);
+    printf("\nNIM 7 = %d", NIM[6]);
+    printf("\nNIM 8 = %d", NIM[7]);
+    printf("\nNIM 9 = %d", NIM[8]);
+    printf("\nNIM 10 = %d", NIM[9]);
+    printf("\nNIM 11= %d", NIM[10]);
+    printf("\nNIM 12= %d", NIM[11]);
+    printf("\nNIM 13= %d", NIM[12]);
+    
+}
