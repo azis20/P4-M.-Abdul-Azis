c6cee7359fc3485e574af68be58594c32d23876e
azis20 <64583776+azis20@users.noreply.github.com>
2020-05-12 14:24:07 +0800

    Create Jobsheet 2 C.c
    Menampilkan array dua dimensi

---
+++ Jobsheet 2 C.c
@@ -0,0 +1,45 @@
+#include <stdio.h> 
+int main() { 
+
+char* NIM [13]; 
+    printf("Input NIM Mahasiswa ke-1 = "); 
+    scanf("%s", &NIM [0]);  
+    printf("Input NIM Mahasiswa ke-2 = ");  
+    scanf("%s", &NIM [1]);  
+    printf("Input NIM Mahasiswa ke-3 = ");  
+    scanf("%s", &NIM [2]); 
+    printf("Input NIM Mahasiswa ke-4 = ");  
+    scanf("%s", &NIM [3]);  
+    printf("Input NIM Mahasiswa ke-5 = ");  
+    scanf("%s", &NIM [4]);  
+    printf("Input NIM Mahasiswa ke-6 = ");  
+    scanf("%s",&NIM [5]);  
+	printf("Input NIM Mahasiswa ke-7 = ");  
+    scanf("%s",&NIM [6]); 
+    printf("Input NIM Mahasiswa ke-8 = ");  
+    scanf("%s",&NIM [7]); 
+    printf("Input NIM Mahasiswa ke-9 = ");  
+    scanf("%s",&NIM [8]); 
+    printf("Input NIM Mahasiswa ke-10 = ");  
+    scanf("%s",&NIM [9]); 
+    printf("Input NIM Mahasiswa ke-11 = ");  
+    scanf("%s",&NIM [10]); 
+    printf("Input NIM Mahasiswa ke-12 = ");  
+    scanf("%s",&NIM [11]); 
+    printf("Input NIM Mahasiswa ke-13 = ");  
+    scanf("%s",&NIM [12]); 
+	printf("\n");
+	printf("NIM Mahasiswa ke-1 adalah %s\n",&NIM[0]);  
+    printf("NIM Mahasiswa ke-2 adalah %s\n",&NIM[1]);  
+	printf("NIM Mahasiswa ke-3 adalah %s\n",&NIM[2]);  
+	printf("NIM Mahasiswa ke-4 adalah %s\n",&NIM[3]); 
+	printf("NIM Mahasiswa ke-5 adalah %s\n",&NIM[4]); 
+	printf("NIM Mahasiswa ke-6 adalah %s\n",&NIM[5]);
+	printf("NIM Mahasiswa ke-7 adalah %s\n",&NIM[6]);
+	printf("NIM Mahasiswa ke-8 adalah %s\n",&NIM[7]);
+	printf("NIM Mahasiswa ke-9 adalah %s\n",&NIM[8]);
+	printf("NIM Mahasiswa ke-10 adalah %s\n",&NIM[9]);
+	printf("NIM Mahasiswa ke-11 adalah %s\n",&NIM[10]);
+	printf("NIM Mahasiswa ke-12 adalah %s\n",&NIM[11]);
+	printf("NIM Mahasiswa ke-13 adalah %s\n",&NIM[12]);
+ } 
