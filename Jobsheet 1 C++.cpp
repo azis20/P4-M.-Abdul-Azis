0c8f5c14c6247d0efcdac44e5f39d55adb458477
azis20 <64583776+azis20@users.noreply.github.com>
2020-05-12 14:22:27 +0800

    Create Jobsheet 1 C++.cpp
    Menampilkan array satu dimensi

---
+++ Jobsheet 1 C++.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout<<"Nama: M.Abdul Azis\n";
+    cout<<"NIM: F1B019083\n";
+	cout<<"Kelompok: 17\n";
+	
+	int NIM[13]= {1,2,3,4,5,6,7,8,9,10,11,12,13};
+	cout<<"NIM 1 ="<<NIM[0]<<endl;
+	cout<<"NIM 2 ="<<NIM[1]<<endl;
+	cout<<"NIM 3 ="<<NIM[2]<<endl;
+	cout<<"NIM 4 ="<<NIM[3]<<endl;
+	cout<<"NIM 5 ="<<NIM[4]<<endl;
+	cout<<"NIM 6 ="<<NIM[5]<<endl;
+	cout<<"NIM 7 ="<<NIM[6]<<endl;
+	cout<<"NIM 8 ="<<NIM[7]<<endl;
+	cout<<"NIM 9 ="<<NIM[8]<<endl;
+	cout<<"NIM 10 ="<<NIM[9]<<endl;
+	cout<<"NIM 11 ="<<NIM[10]<<endl;
+	cout<<"NIM 12 ="<<NIM[11]<<endl;
+	cout<<"NIM 13 ="<<NIM[12]<<endl;
+return 0;
+}
