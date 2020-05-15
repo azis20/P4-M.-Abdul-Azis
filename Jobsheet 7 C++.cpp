9914f58ccfcf8dbbea899ca4cc30089a5186f3d0
azis20 <64583776+azis20@users.noreply.github.com>
2020-05-15 10:44:27 +0800

    Create Jobsheet 7 C++.cpp
    Mengisi Elemen Array menggunaka n perulangan

---
+++ Jobsheet 7 C++.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+main (){
+    int NIM [3];
+    for (int i = 0; i<= 3; i++){
+        cout<< "Masukkan NIM Anda ke-"<<i+1; cout<<"=";
+        cin>>NIM[i];
+    }
+}
