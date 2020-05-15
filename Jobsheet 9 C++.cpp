abb70247b9230bf6028be39cfa7f541df2d617ed
azis20 <64583776+azis20@users.noreply.github.com>
2020-05-15 10:56:36 +0800

    Create Jobsheet 9 C++.cpp
    Array sebagai karakter

---
+++ Jobsheet 9 C++.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+     int i;
+     char nama[5]={'A','Z','I','S'};
+     char nama2[5]="AZIS";
+     cout<<"Array per Karakter= ";
+     or(i=0;i<=4;i++){
+         cout<<nama[i];
+     }
+     cout<<endl; 
+	   cout<<"Array string ="<<nama2<<endl<<endl;
+return 0;
+}
